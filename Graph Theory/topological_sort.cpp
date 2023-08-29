32937c61277bd4b4063fd8e01868cbce13d3e124
Mahmud-Araf <almahmudaraf@gmail.com>
2023-08-29 22:21:24 +0600

    topo_sort modified for detecting cycles

--- Graph Theory/topological_sort.cpp
+++ Graph Theory/topological_sort.cpp
@@ -2,27 +2,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void topo_sort(int v, vector<vector<int>>& adj, vector<bool>& visited, stack<int>& st)
-{
-    
+bool topo_sort(int v, vector<vector<int>>& adj, vector<bool>& visited, stack<int>& st, vector<int>& parent) {
     visited[v] = true;
 
-    for (auto i : adj[v])
-    {
-        if (!visited[i])
-        {
-            topo_sort(i, adj, visited, st);
+    for (auto i : adj[v]) {
+        if (!visited[i]) {
+            parent[i] = v;
+            if (!topo_sort(i, adj, visited, st, parent)) {
+                return false;
+            }
+        } else if (i != parent[v] && visited[i]) {
+            return false;
         }
     }
 
     st.push(v);
+    return true;
 }
+
 
 int main()
 {
     vector<vector<int>> adj(6);
     vector<bool> visited(6, false);
     stack<int> st;
+    vector<int> parent(6, -1);
 
     adj[0].push_back(3);
     adj[0].push_back(4);
@@ -33,7 +37,16 @@ int main()
     for (int i = 0; i < 6; i++)
     {
         if (!visited[i])
-            topo_sort(i, adj, visited, st);
+        {
+            parent[i]=i;
+            if(!topo_sort(i, adj, visited, st,parent))
+            {
+                cout << "Not a DAG" << endl;
+                return 0;
+            }
+        }
+
+            
     }
 
     while (!st.empty())
